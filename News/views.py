9d05f4f235a2e546f26bbca6a8f75459317a9ea7
Nikita Surnachev <nikita.surnachev03@gmail.com>
2018-06-30 15:31:15 +0300

    Changed order of news appearance

--- News/views.py
+++ News/views.py
@@ -4,14 +4,10 @@ from News.models import NewsRecord
 
 def index(request):
     records = NewsRecord.objects.all()
-    AllRecords = {}
     result = {}
-    RecordsStr = ""
 
     result['news'] = []
-    it = 0
     for record in records:
-        result['news'] += [{'title': record.title, 'short_news': record.short_news, 'news': record.news, 'date': record.date},]
-        it += 1
+        result['news'].insert(0, {'title': record.title, 'short_news': record.short_news, 'news': record.news, 'date': record.date})
 
     return render(request, 'News/wrapper.html', result)
