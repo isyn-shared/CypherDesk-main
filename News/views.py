4501180a113a56cf4a815f2910d0f99fbeece29d
Ilya (GitKraken) <ilyuzhaz@gmail.com>
2018-07-01 15:00:45 +0300

    First news overview + WIP Full News

--- News/views.py
+++ News/views.py
@@ -8,6 +8,17 @@ def index(request):
 
     result['news'] = []
     for record in records:
-        result['news'].insert(0, {'title': record.title, 'short_news': record.short_news, 'news': record.news, 'date': record.date})
+        result['news'].insert(0, {'title': record.title, 'short_news': record.short_news, 'news': record.news, 'date': record.date, 'id': record.id})
 
     return render(request, 'News/wrapper.html', result)
+
+def full(request):
+    result = {}
+
+    if (request.GET):
+        id = request.GET['id']
+        post = NewsRecord.objects.filter(id=id)[0]
+        result['post'] = post
+        return render(request, 'News/Full/wrapper.html', result)
+    
+    return HttpResponse('Ошибка 404 - Вы Кек')
