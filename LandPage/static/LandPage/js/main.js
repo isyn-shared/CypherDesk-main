c8ff00150294a2bb2d2e807b5f62ada9a5673048
Ilya Zavyalov <ilyuzhaz@gmail.com>
2018-07-22 22:03:54 +0300

    Фиксы в адаптивности + новый навбар + центровка некоторых страниц

--- LandPage/static/LandPage/js/main.js
+++ LandPage/static/LandPage/js/main.js
@@ -4,7 +4,13 @@ $(document).ready(() => {
         i++;
         if (i == 50)
             clearInterval(timer);
-        else 
-            $(`.show${i}`).fadeIn(700);
-    }, 200);    
+        else {
+            if ($(`.show${i}`).hasClass('showNavbar'))
+                $(`.show${i}`).css('display', 'flex').hide().fadeIn();
+            else
+                $(`.show${i}`).fadeIn(700);
+        }
+    }, 200);
 });
+
+//$span.css('display', 'inline').hide().fadeIn();
