1ecfe6491f792658d1647cffc70016ef46c265da
Ilya Zavyalov <ilyuzhaz@gmail.com>
2018-07-22 20:06:20 +0300

    Минорные фиксы по разным страницам

--- LandPage/static/LandPage/js/main.js
+++ LandPage/static/LandPage/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     let i = 0;
     let timer = setInterval(() => {
         i++;
-        if (i == 5)
+        if (i == 50)
             clearInterval(timer);
         else 
             $(`.show${i}`).fadeIn(700);
