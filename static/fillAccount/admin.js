4113cbdf349e6d9a0339713884734bb90e678b61
Ilya Zavyalov <ilyuzhaz@gmail.com>
2018-08-19 23:32:21 +0300

    Working JS on front-page and fillAdmin

---
+++ static/fillAccount/admin.js
@@ -0,0 +1,43 @@
+function fillAccountAdmin() {
+    const name = $('#nameInput').val(),
+        surname = $('#surnameInput').val(),
+        partonymic = $('#partonymicInput').val(),
+        recourse = $('#recourseInput').val(),
+        mail = $('#mailInput').val(),
+        login = $('#loginInput').val(),
+        pass = $('#passwordInput').val(),
+        repass = $('#repasswordInput').val();
+
+
+    if (!pass || !repass || pass != repass) 
+        createAlert('alert-danger', 'Ошибка!', 'Пароли не совпадают!');
+
+    sendPOST('/fillAdminAccount', {name, surname, partonymic, recourse, mail, login, pass}, true)
+        .then(resp => {
+            if (!resp.ok)
+                return createAlert('alert-danger', 'Ошибка!', resp.err)
+
+            createAlert('alert-success', "Успех!", "Все прошло успешно");
+
+            // TODO: Redirect
+        })
+        .catch(console.error);
+}
+
+function createAlert(type, title, text = "") {
+    $('#alertWrapper').html(`
+        <div class="alert alert-dismissible fade show ${type} mb-0" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Закрыть">
+                <span aria-hidden="true">×</span>
+            </button>
+            <strong>${title}</strong> ${text}
+        </div>
+    `);
+
+    const offset = $('#alertWrapper').offset();
+
+    $('html, body').animate({
+        scrollTop: offset.top,
+        scrollLeft: offset.left
+    });
+}
