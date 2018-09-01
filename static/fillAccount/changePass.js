5210d6272d422ae2bb23fd647989a0a38c82edba
Ilya Zavyalov <ilyuzhaz@gmail.com>
2018-09-01 20:57:58 +0300

    Better color pallet in Admin Panel + frontPage modal + changePass

---
+++ static/fillAccount/changePass.js
@@ -0,0 +1,43 @@
+$(document).ready(() => {
+
+    $('#updatePasswordForm').submit(e => {
+        e.preventDefault();
+
+        const pass = $('#passwordInput').val(),
+            repass = $('#repasswordInput').val();
+
+        if (pass != repass)
+            return createAlert('alert-danger', 'Упс!', 'Пароли не совпадают!');
+
+        // login was saved in HTML after render
+        sendPOST('/remindPass/change', {login, pass})
+            .then(resp => {
+                if (!resp.ok) 
+                    return createAlert('alert-danger', "Упс!", "Произошла ошибка: " + resp.err);
+
+                createAlert('alert-success', "Отлично!", "Данные изменены!");
+            })
+            .catch(err => {
+                createAlert('alert-danger', "Упс!", "Произошла ошибка: " + err);
+                console.error(err);
+            })
+    });
+});
+
+function createAlert(type, title, text, where = "#alertWrapper") {
+    $(where).html(`
+        <div class="alert alert-dismissible fade show ${type} mb-0" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Закрыть">
+                <span aria-hidden="true">×</span>
+            </button>
+            <strong>${title}</strong> ${text}
+        </div>
+    `);
+
+    const offset = $(where).offset();
+
+    $('html, body').animate({
+        scrollTop: offset.top,
+        scrollLeft: offset.left
+    });
+}
--- static/front-page/main.js
+++ static/front-page/main.js
@@ -18,8 +18,8 @@ function login() {
     return false;
 }
 
-function createAlert(type, title, text = "") {
-    $('#alertWrapper').html(`
+function createAlert(type, title, text, where = "#alertWrapperMain") {
+    $(where).html(`
         <div class="alert alert-dismissible fade show ${type} mb-0" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Закрыть">
                 <span aria-hidden="true">×</span>
@@ -28,18 +28,53 @@ function createAlert(type, title, text = "") {
         </div>
     `);
 
-    const offset = $('#alertWrapper').offset();
+    const offset = $(where).offset();
 
     $('html, body').animate({
         scrollTop: offset.top,
         scrollLeft: offset.left
     });
 }
+
+let FPSendingAllowed = true;
+
 $(document).ready(() => {
     $('#loginForm').submit(e => {
         e.preventDefault();
 
         login();
-    })
+    });
+
+    $('#forgotPasswordForm').submit(e => {
+        e.preventDefault();
+        if (!FPSendingAllowed) return;
+
+        const credentials = $('#FPCredInput').val();
+
+        $('#FPSubmit').addClass('disabled');
+        FPSendingAllowed = false;
+
+        createAlert('alert-info', 'Загрузка...', 'Пожалуйста подождите...', '#alertWrapperFP');
+
+        sendPOST('/remindPass', {credentials})
+            .then(resp => {
+                allowForm();
+
+                if (!resp.ok) 
+                    return createAlert('alert-danger', "Упс!", "Произошла ошибка: " + resp.err, '#alertWrapperFP');
+
+                createAlert('alert-success', "Отлично!", "Запрос был отправлен!", '#alertWrapperFP');
+            })
+            .catch(err => {
+                allowForm();
+                createAlert('alert-danger', "Упс!", "Произошла ошибка: " + err, '#alertWrapperFP');
+                console.error(err);
+            });
+
+        function allowForm() {
+            FPSendingAllowed = true;
+            $('#FPSubmit').removeClass('disabled');
+        }
+    });
 
 });
